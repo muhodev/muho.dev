51514b91d97fdd17ac7290fcac1246d770dfb74d
muho <hi@muho.dev>
2020-09-13 22:09:08 +0300

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,29 @@ module.exports = {
     },
     "gatsby-plugin-sitemap",
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.YOUR_GOOGLE_ANALYTICS_TRACKING_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "muho.dev",
+      },
+    },
+    {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: process.env.STRAPI_API_URL || "http://localhost:1337",
